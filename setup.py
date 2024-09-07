f34cd6d1c076616c8d9a5793aceccdbb029fa530
FrenchCommando <martialren@gmail.com>
2024-09-07 17:40:26 -0400

    requirements and version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="log_ip",
+    name="bubbypdf",
     version="0.0.1",
     author="FrenchCommando",
     author_email="martialren@gmail.com",
