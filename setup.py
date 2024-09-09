0735b5f0ce38e04b1545dc68a9e06a005d18af9e
FrenchCommando <martialren@gmail.com>
2024-09-08 21:54:33 -0400

    version 003 and deps

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bubbypdf",
-    version="0.0.2",
+    version="0.0.3",
     author="FrenchCommando",
     author_email="martialren@gmail.com",
     description="A script that merges pdf files",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/FrenchCommando/bubbypdf",
     packages=setuptools.find_packages(),
+    install_requires=['pdfrw==0.4'],
     classifiers=[
         "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
